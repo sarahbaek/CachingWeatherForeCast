9b4c7815d5f90266051b1e1807044c985c2bc7ca
sarahBæk2021 <sbaek18@gmail.com>
2022-01-04 16:53:00 +0100

    Add project files.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace CachingWeatherForeCast.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IMemoryCache _memoryCache;
+        private readonly string weatherForecastGet = "weatherForecastKey";
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IMemoryCache memoryCache)
+        {
+            _logger = logger;
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            IEnumerable<WeatherForecast> weatherForecastCollection = null;
+            if (_memoryCache.TryGetValue(weatherForecastGet, out weatherForecastCollection))
+            {
+                return weatherForecastCollection;
+            }
+            else
+            {
+                weatherForecastCollection = CreateWeatherForecast();
+                //save it in the memory
+              MemoryCacheEntryOptions cacheOptions =  new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromSeconds(30));
+              _memoryCache.Set(weatherForecastGet, weatherForecastCollection, cacheOptions);
+              return weatherForecastCollection;
+
+            }
+        }
+
+        public IEnumerable<WeatherForecast> CreateWeatherForecast()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+        }
+    }
+}
